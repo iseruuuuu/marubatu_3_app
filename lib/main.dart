baf18bab6a0cf58c5e112d159054c3f69fc75cc7
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-08 18:12:13 +0900

    add: add getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:barubatu_3_app/home_screen.dart';
+import 'package:barubatu_3_app/screen/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
-
