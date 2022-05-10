cd1a63f591d1aaaefb60fe2b60acf1f98571ccac
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-10 19:44:16 +0900

    add: Preferenceを追加

--- lib/screen/challenge_list_screen/challenge_list_screen_controller.dart
+++ lib/screen/challenge_list_screen/challenge_list_screen_controller.dart
@@ -1,11 +1,33 @@
 import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_3.dart';
 import 'package:barubatu_3_app/screen/trophy_screen/trophy_screen.dart';
+import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
+import '../../preference/shared_preference.dart';
 import '../challenge_screen/challenge_screen_1.dart';
 import '../challenge_screen/challenge_screen_2.dart';
 import '../challenge_screen/challenge_screen_5.dart';
 
 class ChallengeScreenController extends GetxController {
+  var stage1 = false.obs;
+  var stage2 = false.obs;
+  var stage3 = false.obs;
+  var stage4 = false.obs;
+  var stage5 = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    sharedPreference();
+  }
+
+  Future<void> sharedPreference() async {
+    stage1.value = await Preference().getBool(PreferenceKey.stage1);
+    stage2.value = await Preference().getBool(PreferenceKey.stage2);
+    stage3.value = await Preference().getBool(PreferenceKey.stage3);
+    stage4.value = await Preference().getBool(PreferenceKey.stage4);
+    stage5.value = await Preference().getBool(PreferenceKey.stage5);
+  }
+
   void onTapLevel1() {
     Get.to(() => const ChallengeScreen1());
   }
@@ -27,6 +49,14 @@ class ChallengeScreenController extends GetxController {
   }
 
   void onTapTrophyScreen() {
-    Get.to(() => const TrophyScreen());
+    Get.to(
+      () => TrophyScreen(
+        isClear1: stage1.value,
+        isClear2: stage2.value,
+        isClear3: stage3.value,
+        isClear4: stage4.value,
+        isClear5: stage5.value,
+      ),
+    );
   }
 }
