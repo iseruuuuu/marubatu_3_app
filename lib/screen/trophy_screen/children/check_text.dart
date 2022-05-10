d239670d57958817132e3cbfd5c71cd9f107621c
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-10 19:44:31 +0900

    add: トロフィー画面のテキストのアイテムを作成

---
+++ lib/screen/trophy_screen/children/check_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CheckText extends StatelessWidget {
+  const CheckText({
+    Key? key,
+    required this.isOpen,
+  }) : super(key: key);
+
+  final bool isOpen;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width / 6,
+      child: Center(
+        child: !isOpen
+            ? const Text(
+                '未所持',
+              )
+            : const Text('獲得済み'),
+      ),
+    );
+  }
+}
