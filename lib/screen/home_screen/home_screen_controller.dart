90e9343b78c253d880009969cbb15a205272729b
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-14 19:08:10 +0900

    add: home画面のBGMを追加した

--- lib/screen/home_screen/home_screen_controller.dart
+++ lib/screen/home_screen/home_screen_controller.dart
@@ -13,36 +13,59 @@ class HomeScreenController extends GetxController
   static const tapSound = 'images/tap.mp3';
   static const backgroundMusic = 'images/background.mp3';
   final AudioCache _cache = AudioCache(fixedPlayer: AudioPlayer());
+  AudioPlayer? _player;
 
   @override
   void onInit() {
     super.onInit();
-
     final newVersion = NewVersion(
       androidId: 'com.barubatu_3_app',
       iOSId: 'com.barubatu3App',
       iOSAppStoreCountry: 'JP',
     );
     openUpdateDialog(newVersion);
-
     animationController = AnimationController(
       vsync: this,
       duration: const Duration(
         seconds: 1,
       ),
     )..repeat(reverse: true);
-
+    bgmPlayer(name: backgroundMusic);
     loadSound();
-    _cache.loop(backgroundMusic);
+  }
+
+  void bgmPlayer({required String name, bool isLoop = true}) {
+    () async {
+      await _player?.stop();
+      await _player?.dispose();
+      if (isLoop) {
+        _player = await _cache.loop(name, mode: PlayerMode.MEDIA_PLAYER);
+      } else {
+        _player = await _cache.play(name, mode: PlayerMode.MEDIA_PLAYER);
+      }
+    }();
   }
 
   void loadSound() async {
     _cache.load(tapSound);
-    _cache.load(backgroundMusic);
   }
 
   void playSound() async {
     _cache.play(tapSound);
+  }
+
+  void stopBgm() async {
+    await _player?.stop();
+  }
+
+  Future<void> disposeBgm() async {
+    return await _player?.dispose();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    stopBgm();
   }
 
   void openUpdateDialog(NewVersion newVersion) async {
@@ -66,15 +89,18 @@ class HomeScreenController extends GetxController
   void onTap() {
     Get.to(() => const GameScreen());
     playSound();
+    stopBgm();
   }
 
   void onTapChallenge() {
     Get.to(() => const ChallengeListScreen());
     playSound();
+    stopBgm();
   }
 
   void onTapSetting() {
     Get.to(() => const SettingScreen());
     playSound();
+    stopBgm();
   }
 }
