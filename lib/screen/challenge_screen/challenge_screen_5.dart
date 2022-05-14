c1f1e3fe8b834592814fd8230d083c94f388a9fc
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-14 19:05:42 +0900

    add: チャレンジ５画面のBGMを追加

--- lib/screen/challenge_screen/challenge_screen_5.dart
+++ lib/screen/challenge_screen/challenge_screen_5.dart
@@ -101,6 +101,8 @@ class _ChallengeScreen5State extends State<ChallengeScreen5> {
   void initState() {
     super.initState();
     openFirstDialog();
+    bgmPlayer(name: backgroundMusic);
+    loadSound();
   }
 
   void openFirstDialog() {
@@ -179,6 +181,35 @@ class _ChallengeScreen5State extends State<ChallengeScreen5> {
   static const gameClear = 'images/game_clear.mp3';
   static const noGameClear = 'images/no_game_clear.mp3';
   final AudioCache _cache = AudioCache(fixedPlayer: AudioPlayer());
+
+  static const backgroundMusic = 'images/game_bgm.mp3';
+  AudioPlayer? _player;
+
+  void bgmPlayer({required String name, bool isLoop = true}) {
+        () async {
+      await _player?.stop();
+      await _player?.dispose();
+      if (isLoop) {
+        _player = await _cache.loop(name, mode: PlayerMode.MEDIA_PLAYER);
+      } else {
+        _player = await _cache.play(name, mode: PlayerMode.MEDIA_PLAYER);
+      }
+    }();
+  }
+
+  void stopBgm() async {
+    await _player?.stop();
+  }
+
+  Future<void> disposeBgm() async {
+    return await _player?.dispose();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    stopBgm();
+  }
 
   void loadSound() async {
     _cache.load(tapSound);
