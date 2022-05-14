b1f0c230ad782e08a82170439ae0f717163eb8ff
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-14 19:08:54 +0900

    add: トロフィー画面にBMGを追加した

--- lib/screen/trophy_screen/trophy_screen.dart
+++ lib/screen/trophy_screen/trophy_screen.dart
@@ -1,4 +1,3 @@
-import 'package:barubatu_3_app/screen/trophy_screen/children/check_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
@@ -29,49 +28,39 @@ class TrophyScreen extends StatefulWidget {
 class _TrophyScreenState extends State<TrophyScreen> with RouteAware {
   static const backgroundMusic = 'images/trophy_b.mp3';
   final AudioCache _cache = AudioCache(fixedPlayer: AudioPlayer());
-  final AudioCache _player = AudioCache();
+  AudioPlayer? _player;
 
-  static const audioFilesPrefix = 'audio/';
-  final audioPlayer = AudioPlayer();
-  AudioPlayer player = AudioPlayer();
-  //final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+  void bgmPlayer({required String name, bool isLoop = true}) {
+    () async {
+      await _player?.stop();
+      await _player?.dispose();
+      if (isLoop) {
+        _player = await _cache.loop(name, mode: PlayerMode.MEDIA_PLAYER);
+      } else {
+        _player = await _cache.play(name, mode: PlayerMode.MEDIA_PLAYER);
+      }
+    }();
+  }
+
+  void stopBgm() async {
+    await _player?.stop();
+  }
+
+  Future<void> disposeBgm() async {
+    return await _player?.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
-    //_cache.loop(backgroundMusic, isNotification: true,mode: PlayerMode.LOW_LATENCY);
-    //playSound();
-
+    bgmPlayer(name: backgroundMusic);
   }
 
-  void playSound() async {
-    // AudioPlayer _ap = await _player.loop(backgroundMusic);
-    await audioPlayer.play(backgroundMusic);
-    player.play(backgroundMusic);
+  @override
+  void dispose() {
+    super.dispose();
+    stopBgm();
   }
-
-  void stopSound() async {
-    audioPlayer.stop();
-  }
-
-  //
-  // @override
-  // void didChangeDependencies() {
-  //   super.didChangeDependencies();
-  //   routeObserver.subscribe(this, ModalRoute.of(context));
-  // }
-  //
-  // @override
-  // void dispose() {
-  //   routeObserver.unsubscribe(this);
-  //   super.dispose();
-  // }
-  //
-  // @override
-  // void didPop(){
-  //   print('sa');
-  //   _cache.clearAll();
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -95,56 +84,14 @@ class _TrophyScreenState extends State<TrophyScreen> with RouteAware {
               size: 35,
             ),
             onPressed: () {
-              stopSound();
+              stopBgm();
               Navigator.pop(context);
-              print('sa');
             },
           ),
         ),
       ),
       body: Column(
         children: [
-          const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              widget.isClear1
-                  ? Assets.images.gold1
-                      .image(width: deviceSize / 6, height: deviceSize / 6)
-                  : Assets.images.grey1
-                      .image(width: deviceSize / 6, height: deviceSize / 6),
-              widget.isClear2
-                  ? Assets.images.gold2
-                      .image(width: deviceSize / 6, height: deviceSize / 6)
-                  : Assets.images.grey2
-                      .image(width: deviceSize / 6, height: deviceSize / 6),
-              widget.isClear3
-                  ? Assets.images.gold3
-                      .image(width: deviceSize / 6, height: deviceSize / 6)
-                  : Assets.images.grey3
-                      .image(width: deviceSize / 6, height: deviceSize / 6),
-              widget.isClear4
-                  ? Assets.images.gold4
-                      .image(width: deviceSize / 6, height: deviceSize / 6)
-                  : Assets.images.grey4
-                      .image(width: deviceSize / 6, height: deviceSize / 6),
-              widget.isClear5
-                  ? Assets.images.gold5
-                      .image(width: deviceSize / 6, height: deviceSize / 6)
-                  : Assets.images.grey5
-                      .image(width: deviceSize / 6, height: deviceSize / 6),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CheckText(isOpen: widget.isClear1),
-              CheckText(isOpen: widget.isClear2),
-              CheckText(isOpen: widget.isClear3),
-              CheckText(isOpen: widget.isClear4),
-              CheckText(isOpen: widget.isClear5),
-            ],
-          ),
           const Spacer(),
           ImageSlideshow(
             autoPlayInterval: 3000,
