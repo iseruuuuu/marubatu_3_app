3325d06639f84554eb97e061c02bcd2feb564396
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-21 21:08:44 +0900

    add: チャレンジ３画面の「◯のターン」の表示のデザインに手を加えた

--- lib/screen/challenge_screen/challenge_screen_3.dart
+++ lib/screen/challenge_screen/challenge_screen_3.dart
@@ -18,8 +18,6 @@ class ChallengeScreen3 extends StatefulWidget {
 }
 
 class _ChallengeScreen3State extends State<ChallengeScreen3> {
-  // final controller = Get.put(GameScreenController(), tag: '');
-
   //まる　　  1 = true, 　　  2 = false,　　  3 = false,
   //ばつ　　  1 = false,　　  2 = true,　　  3 = false,
   //四角　　  1 = false, 　　  2 = false,　　  3 = true,
@@ -31,6 +29,16 @@ class _ChallengeScreen3State extends State<ChallengeScreen3> {
   List<Widget> buildLine = [Container()];
   double lineThickness = 5.0;
   late double lineWidth;
+  int gameCount = 0;
+  bool isClear = false;
+  static const tapSound = 'images/game_tap.mp3';
+  static const gameClear = 'images/game_clear.mp3';
+  static const noGameClear = 'images/no_game_clear.mp3';
+  final AudioCache _cache = AudioCache(fixedPlayer: AudioPlayer());
+
+  // static const backgroundMusic = 'images/game_bgm.mp3';
+  AudioPlayer? _player;
+
   final List<List<int>> settlementListHorizontal = [
     [0, 1, 2],
     [1, 2, 3],
@@ -98,8 +106,6 @@ class _ChallengeScreen3State extends State<ChallengeScreen3> {
     [14, 18, 22],
   ];
 
-  int gameCount = 0;
-
   @override
   void initState() {
     super.initState();
@@ -126,26 +132,16 @@ class _ChallengeScreen3State extends State<ChallengeScreen3> {
   }
 
   void clear() {
-    setState(
-      () {
-        turnOfPlayer1 = true;
-        turnOfPlayer2 = false;
-        turnOfPlayer3 = false;
-        statusList = List.filled(25, PieceStatus.none);
-        gameStatus = GameStatus.play;
-        buildLine = [Container()];
-        gameCount = 0;
-      },
-    );
+    setState(() {
+      turnOfPlayer1 = true;
+      turnOfPlayer2 = false;
+      turnOfPlayer3 = false;
+      statusList = List.filled(25, PieceStatus.none);
+      gameStatus = GameStatus.play;
+      buildLine = [Container()];
+      gameCount = 0;
+    });
   }
-
-  static const tapSound = 'images/game_tap.mp3';
-  static const gameClear = 'images/game_clear.mp3';
-  static const noGameClear = 'images/no_game_clear.mp3';
-  final AudioCache _cache = AudioCache(fixedPlayer: AudioPlayer());
-
-  // static const backgroundMusic = 'images/game_bgm.mp3';
-  AudioPlayer? _player;
 
   // void bgmPlayer({required String name, bool isLoop = true}) {
   //       () async {
@@ -200,8 +196,9 @@ class _ChallengeScreen3State extends State<ChallengeScreen3> {
         leading: Padding(
           padding: const EdgeInsets.only(left: 10),
           child: IconButton(
+            iconSize: 45,
             icon: const Icon(
-              Icons.arrow_back_ios,
+              Icons.arrow_back,
               color: Colors.blueAccent,
               size: 35,
             ),
@@ -238,79 +235,91 @@ class _ChallengeScreen3State extends State<ChallengeScreen3> {
   Widget buildRow() {
     switch (gameStatus) {
       case GameStatus.play:
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            turnOfPlayer1
-                ? const Icon(
-                    FontAwesomeIcons.circle,
-                    size: 50,
-                    color: ColorStyle.blue,
-                  )
-                : Container(),
-            turnOfPlayer2
-                ? const Icon(
-                    Icons.clear,
-                    size: 60,
-                    color: ColorStyle.red,
-                  )
-                : Container(),
-            turnOfPlayer3
-                ? const Icon(
-                    FontAwesomeIcons.square,
-                    color: ColorStyle.green,
-                    size: 50,
-                  )
-                : Container(),
-            const SizedBox(width: 5),
-            const Text(
-              'のターン',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 45,
+        return Material(
+          elevation: 10,
+          child: Container(
+            width: MediaQuery.of(context).size.width - 50,
+            height: 100.w,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 3,
+                color: Colors.grey.shade700,
               ),
             ),
-          ],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                turnOfPlayer1
+                    ? const Icon(
+                        FontAwesomeIcons.circle,
+                        size: 80,
+                        color: ColorStyle.blue,
+                      )
+                    : const Icon(
+                        FontAwesomeIcons.circle,
+                        size: 80,
+                        color: Colors.grey,
+                      ),
+                turnOfPlayer2
+                    ? const Icon(
+                        Icons.clear,
+                        size: 100,
+                        color: ColorStyle.red,
+                      )
+                    : const Icon(
+                        Icons.clear,
+                        size: 100,
+                        color: Colors.grey,
+                      ),
+                turnOfPlayer3
+                    ? const Icon(
+                        FontAwesomeIcons.square,
+                        color: ColorStyle.green,
+                        size: 80,
+                      )
+                    : const Icon(
+                        FontAwesomeIcons.square,
+                        color: Colors.grey,
+                        size: 80,
+                      ),
+              ],
+            ),
+          ),
         );
       case GameStatus.draw:
         return const Text("引き分けです", style: TextStyle(fontSize: 30));
       case GameStatus.settlement:
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            turnOfPlayer1
-                ? const Icon(
-                    FontAwesomeIcons.square,
-                    color: ColorStyle.green,
-                    size: 50,
-                  )
-                : Container(),
-            turnOfPlayer2
-                ? const Icon(
-                    FontAwesomeIcons.circle,
-                    size: 50,
-                    color: ColorStyle.blue,
-                  )
-                : Container(),
-            turnOfPlayer3
-                ? const Icon(
-                    Icons.clear,
-                    size: 60,
-                    color: ColorStyle.red,
-                  )
-                : Container(),
-            const SizedBox(
-              width: 5,
-            ),
-            const Text(
-              "の勝ち",
-              style: TextStyle(
-                fontSize: 45,
-                color: ColorStyle.textColor,
+        if (isClear) {
+          return Material(
+            elevation: 10,
+            child: Container(
+              width: MediaQuery.of(context).size.width - 50,
+              height: 100.w,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 3,
+                  color: Colors.grey.shade700,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.star, color: Colors.yellow, size: 50),
+                  Text(
+                    'ゲームクリア',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  Icon(Icons.star, color: Colors.yellow, size: 50),
+                ],
               ),
             ),
-          ],
-        );
+          );
+        } else {
+          return SizedBox(
+            width: MediaQuery.of(context).size.width - 50,
+            height: 100.w,
+          );
+        }
       default:
         return Container();
     }
@@ -380,11 +389,7 @@ class _ChallengeScreen3State extends State<ChallengeScreen3> {
           ),
         );
       }
-      _columnChildren.add(
-        Row(
-          children: _rowChildren,
-        ),
-      );
+      _columnChildren.add(Row(children: _rowChildren));
       _columnChildren.add(
         const Divider(
           height: 0.0,
@@ -511,29 +516,31 @@ class _ChallengeScreen3State extends State<ChallengeScreen3> {
   void openWinningDialog(bool isWin) {
     String whoWin = '';
     DialogType dialogType = DialogType.ERROR;
+    var dismiss = false;
     if (gameCount == 15) {
       clearGame();
       whoWin = 'ゲームクリア';
       dialogType = DialogType.SUCCES;
+      dismiss = true;
+      isClear = true;
       setPreference();
     } else {
       noClearGame();
       whoWin = 'クリア失敗';
       dialogType = DialogType.ERROR;
-      //TODO 次のレベルが解放される or 何かすごいものをみせる
+      dismiss = false;
     }
-
     if (isWin) {
       noClearGame();
       whoWin = 'クリア失敗';
       dialogType = DialogType.ERROR;
+      dismiss = false;
     }
-
     AwesomeDialog(
       context: context,
       dialogType: dialogType,
       barrierColor: Colors.grey.shade100,
-      dismissOnTouchOutside: true,
+      dismissOnTouchOutside: dismiss,
       animType: AnimType.SCALE,
       title: whoWin,
       titleTextStyle: TextStyle(
@@ -544,12 +551,10 @@ class _ChallengeScreen3State extends State<ChallengeScreen3> {
       btnOkText: 'もう一度',
       btnCancelText: 'タイトル',
       btnCancelOnPress: () {
-        //タイトルへ
         Navigator.pop(context);
         Navigator.pop(context);
       },
       btnOkOnPress: () {
-        //リセット
         clear();
       },
     ).show();
