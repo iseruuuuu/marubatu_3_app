cca3d9b07f51b2a5e0ccac99f1353253712f00a8
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-01-05 10:23:23 +0900

    add: チャレンジリスト画面のUIのリファクタリングをしました

--- lib/screen/challenge_list_screen/challenge_list_screen.dart
+++ lib/screen/challenge_list_screen/challenge_list_screen.dart
@@ -1,109 +1,231 @@
-// Flutter imports:
+import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_3.dart';
 import 'package:flutter/material.dart';
-
-// Package imports:
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
-// Project imports:
-import 'package:barubatu_3_app/screen/home_screen/children/button_item.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'challenge_list_screen_controller.dart';
+import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_1.dart';
+import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_2.dart';
+import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_4.dart';
+import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_5.dart';
+import 'package:barubatu_3_app/screen/trophy_screen/trophy_screen.dart';
 
 class ChallengeListScreen extends StatelessWidget {
-  const ChallengeListScreen({Key? key}) : super(key: key);
+  const ChallengeListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(ChallengeScreenController(), tag: '');
     return Scaffold(
-      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black.withValues(alpha: 0.25),
         leading: IconButton(
           iconSize: 35.w,
           icon: const Icon(
             Icons.arrow_back_ios,
-            color: Colors.blueAccent,
+            color: Color(0xFF00FFA3),
           ),
           onPressed: controller.onTapBack,
         ),
         actions: [
           IconButton(
-            onPressed: controller.onTapTrophyScreen,
+            onPressed: () {
+              Get.to(
+                () => TrophyScreen(
+                  isClear1: controller.stage1.value,
+                  isClear2: controller.stage2.value,
+                  isClear3: controller.stage3.value,
+                  isClear4: controller.stage4.value,
+                  isClear5: controller.stage5.value,
+                ),
+              );
+            },
             iconSize: 40.w,
             icon: const Icon(
               Icons.flag,
-              color: Colors.black,
+              color: Color(0xFF00FFA3),
             ),
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ScaleTransition(
-              scale: controller.animationController.drive(
-                Tween<double>(
-                  begin: 1,
-                  end: 1.1,
-                ),
-              ),
-              child: ButtonItem(
-                onPressed: controller.onTapLevel1,
-                text: 'レベル１',
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/home/background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ScaleTransition(
+                    scale: controller.animationController.drive(
+                      Tween<double>(
+                        begin: 1,
+                        end: 1.1,
+                      ),
+                    ),
+                    child: _MenuButton(
+                      label: 'レベル１',
+                      onPressed: () {
+                        Get.to(() => const ChallengeScreen1());
+                      },
+                    ),
+                  ),
+                  ScaleTransition(
+                    scale: controller.animationController.drive(
+                      Tween<double>(
+                        begin: 1,
+                        end: 1.13,
+                      ),
+                    ),
+                    child: _MenuButton(
+                      label: 'レベル２',
+                      onPressed: () {
+                        Get.to(() => const ChallengeScreen2());
+                      },
+                    ),
+                  ),
+                  ScaleTransition(
+                    scale: controller.animationController.drive(
+                      Tween<double>(
+                        begin: 1,
+                        end: 1.17,
+                      ),
+                    ),
+                    child: _MenuButton(
+                      label: 'レベル３',
+                      onPressed: () {
+                        Get.to(() => const ChallengeScreen3());
+                      },
+                    ),
+                  ),
+                  ScaleTransition(
+                    scale: controller.animationController.drive(
+                      Tween<double>(
+                        begin: 1,
+                        end: 1.17,
+                      ),
+                    ),
+                    child: _MenuButton(
+                      label: 'レベル４',
+                      onPressed: () {
+                        Get.to(() => const ChallengeScreen4());
+                      },
+                    ),
+                  ),
+                  ScaleTransition(
+                    scale: controller.animationController.drive(
+                      Tween<double>(
+                        begin: 1,
+                        end: 1.2,
+                      ),
+                    ),
+                    child: _MenuButton(
+                      label: 'レベル５',
+                      onPressed: () {
+                        Get.to(() => const ChallengeScreen5());
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-            ScaleTransition(
-              scale: controller.animationController.drive(
-                Tween<double>(
-                  begin: 1,
-                  end: 1.13,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MenuButton extends StatelessWidget {
+  const _MenuButton({required this.label, required this.onPressed});
+  final String label;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final accent = const Color(0xFF00FFA3);
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.82,
+      height: 64,
+      child: Material(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(22),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(22),
+          onTap: onPressed,
+          child: Ink(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(22),
+              gradient: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF0B1220),
+                  Color(0xFF0F172A),
+                ],
+              ),
+              border:
+                  Border.all(color: accent.withValues(alpha: 0.6), width: 2),
+              boxShadow: [
+                BoxShadow(
+                  color: accent.withValues(alpha: 0.25),
+                  blurRadius: 24,
+                  spreadRadius: 2,
                 ),
-              ),
-              child: ButtonItem(
-                onPressed: controller.onTapLevel2,
-                text: 'レベル２',
-              ),
-            ),
-            ScaleTransition(
-              scale: controller.animationController.drive(
-                Tween<double>(
-                  begin: 1,
-                  end: 1.15,
+                BoxShadow(
+                  color: accent.withValues(alpha: 0.10),
+                  blurRadius: 48,
+                  spreadRadius: 10,
                 ),
-              ),
-              child: ButtonItem(
-                onPressed: controller.onTapLevel3,
-                text: 'レベル３',
-              ),
+              ],
             ),
-            ScaleTransition(
-              scale: controller.animationController.drive(
-                Tween<double>(
-                  begin: 1,
-                  end: 1.17,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      height: 12,
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 6,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.white.withValues(alpha: 0.08),
+                            Colors.white.withValues(alpha: 0.0),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              child: ButtonItem(
-                onPressed: controller.onTapLevel4,
-                text: 'レベル４',
-              ),
-            ),
-            ScaleTransition(
-              scale: controller.animationController.drive(
-                Tween<double>(
-                  begin: 1,
-                  end: 1.2,
+                Center(
+                  child: Text(
+                    label,
+                    style: GoogleFonts.mPlusRounded1c(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
                 ),
-              ),
-              child: ButtonItem(
-                onPressed: controller.onTapLevel5,
-                text: 'レベル５',
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/screen/challenge_list_screen/challenge_list_screen_controller.dart
+++ lib/screen/challenge_list_screen/challenge_list_screen_controller.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-
-// Package imports:
-import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
-
-// Project imports:
 import 'package:barubatu_3_app/preference/shared_preference.dart';
-import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_1.dart';
-import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_2.dart';
-import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_3.dart';
-import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_4.dart';
-import 'package:barubatu_3_app/screen/challenge_screen/challenge_screen_5.dart';
-import 'package:barubatu_3_app/screen/trophy_screen/trophy_screen.dart';
 
 class ChallengeScreenController extends GetxController
     with GetSingleTickerProviderStateMixin, WidgetsBindingObserver, RouteAware {
@@ -23,13 +12,6 @@ class ChallengeScreenController extends GetxController
 
   late AnimationController animationController;
 
-  // static const bgm = 'images/background.mp3';
-  // final AudioCache bgmCache = AudioCache(fixedPlayer: AudioPlayer());
-  // AudioPlayer? bgmPlayer;
-
-  static const tap = 'images/tap.mp3';
-  final AudioPlayer tapPlayer = AudioPlayer();
-
   @override
   void onInit() {
     super.onInit();
@@ -40,7 +22,6 @@ class ChallengeScreenController extends GetxController
         seconds: 1,
       ),
     )..repeat(reverse: true);
-    loadTap();
     WidgetsBinding.instance.addObserver(this);
   }
 
@@ -52,50 +33,14 @@ class ChallengeScreenController extends GetxController
     super.dispose();
   }
 
-  // @override
-  // void didChangeAppLifecycleState(AppLifecycleState state) {
-  //   if (state == AppLifecycleState.resumed) {
-  //     onForeground();
-  //   } else if (state == AppLifecycleState.paused) {
-  //     onBackground();
-  //   }
-  // }
-
-  // void loadBgm({required String name, bool isLoop = true}) {
-  //   () async {
-  //     await bgmPlayer?.stop();
-  //     await bgmPlayer?.dispose();
-  //     bgmPlayer = await bgmCache.loop(name, mode: PlayerMode.MEDIA_PLAYER);
-  //   }();
-  // }
-
-  void loadTap() async {
-    await tapPlayer.setPlayerMode(PlayerMode.lowLatency);
-  }
-
-  void playTap() async {
-    await tapPlayer.stop();
-    await tapPlayer.play(AssetSource(tap));
-  }
-
-  // void onBackground() {
-  //   bgmPlayer?.pause();
-  // }
-  //
-  // void onForeground() {
-  //   bgmPlayer?.resume();
-  // }
-
   @override
   void onClose() {
-    tapPlayer.dispose();
     animationController.dispose();
     super.onClose();
   }
 
   void onTapBack() {
     Get.back();
-    // onBackground();
   }
 
   Future<void> sharedPreference() async {
@@ -104,61 +49,5 @@ class ChallengeScreenController extends GetxController
     stage3.value = await Preference().getBool(PreferenceKey.stage3);
     stage4.value = await Preference().getBool(PreferenceKey.stage4);
     stage5.value = await Preference().getBool(PreferenceKey.stage5);
-  }
-
-  void onTapLevel1() {
-    Get.to(() => const ChallengeScreen1())?.then((value) {
-      // onForeground();
-    });
-    // onBackground();
-    playTap();
-  }
-
-  void onTapLevel2() {
-    Get.to(() => const ChallengeScreen2())?.then((value) {
-      // onForeground();
-    });
-    // onBackground();
-    playTap();
-  }
-
-  void onTapLevel3() {
-    Get.to(() => const ChallengeScreen3())?.then((value) {
-      // onForeground();
-    });
-    // onBackground();
-    playTap();
-  }
-
-  void onTapLevel4() {
-    Get.to(() => const ChallengeScreen4())?.then((value) {
-      // onForeground();
-    });
-    // onBackground();
-    playTap();
-  }
-
-  void onTapLevel5() {
-    Get.to(() => const ChallengeScreen5())?.then((value) {
-      // onForeground();
-    });
-    // onBackground();
-    playTap();
-  }
-
-  void onTapTrophyScreen() {
-    Get.to(
-      () => TrophyScreen(
-        isClear1: stage1.value,
-        isClear2: stage2.value,
-        isClear3: stage3.value,
-        isClear4: stage4.value,
-        isClear5: stage5.value,
-      ),
-    )?.then((value) {
-      // onForeground();
-    });
-    // onBackground();
-    playTap();
   }
 }
